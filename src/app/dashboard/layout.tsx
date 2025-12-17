bfc566a6463483f0dafb09b583e0220d840a0c87
qq1256807020 <1256807020@qq.com>
2025-12-17 16:51:48 +0800

    feat: 高亮tab

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -1,6 +1,7 @@
 "use client"
 import Link from "next/link";
 import { useState } from "react";
+import { usePathname } from 'next/navigation'
 
 export default function DashboardLayout({
   children,
@@ -8,12 +9,13 @@ export default function DashboardLayout({
   children: React.ReactNode;
 }>) {
   const [count, setCount] = useState(0)
+  const pathname = usePathname()
+  console.log(pathname)
   return (
     <div className="border-2 border-dashed border-black p-4 w-1/2 mx-auto mt-10">
-      <div className="flex gap-4 font-bold text-lg text-purple-500">
-        <Link href="/dashboard/about">About</Link>
-        {/* <span className="p-4">|</span> */}
-        <Link href="/dashboard/settings">Settings</Link>
+      <div className="flex gap-4 font-bold text-lg ">
+        <Link className={pathname === '/dashboard/about'?"text-purple-500":""} href="/dashboard/about">About</Link>
+        <Link className={pathname === '/dashboard/settings'?"text-purple-500":""} href="/dashboard/settings">Settings</Link>
       </div>
       <h2>dashboard layout {count}</h2>
       <button className="bg-black text-white p-2 rounded-md" onClick={()=>setCount(count +1)}>点击增加+</button>
