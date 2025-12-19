fba8c34bf6f63d061b368853572c34bfeea9eb1c
qq1256807020 <1256807020@qq.com>
2025-12-19 13:34:21 +0800

    feat: get缓存退出

--- src/app/api/time/route.ts
+++ src/app/api/time/route.ts
@@ -1,11 +1,18 @@
+import { cookies, headers } from "next/headers"
 import { NextRequest, NextResponse } from "next/server"
 
-export function GET(request:NextRequest){
+export function GET(request: NextRequest) {
   console.log('GET /api/time')
   const searchParams = request.nextUrl.searchParams
+  const cookieStore = cookies()
+  const token = cookieStore.get('token')
+  const headersLit = headers()
+  const referer = headersLit.get('referer')
   // 有缓存,观察缓存先执行build再启动start
   return NextResponse.json({
-    time:new Date().toLocaleTimeString(),
-    searchParams
+    time: new Date().toLocaleTimeString(),
+    searchParams,
+    token,
+    referer
   })
 }
